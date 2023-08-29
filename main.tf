f5c585389cdad87da9357e92814e023796851f0e
Frederic LEROY <fleroy@synergy.fr>
2023-08-29 16:15:50 +0200

    Ajout Schéma

--- main.tf
+++ main.tf
@@ -22,3 +22,8 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
