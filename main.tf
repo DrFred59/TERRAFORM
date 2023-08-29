72f407acf68daa80406ca02822671fea292aea68
DrFred59 <143401352+DrFred59@users.noreply.github.com>
2023-08-29 10:18:16 +0200

    Create snowflake-terraform-demo_2.yml

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "TEST_SYNERGY"
+
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
