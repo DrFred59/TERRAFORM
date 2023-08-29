f16f35597b2303287b25b9ab2a8b5207dc6fc898
Frederic LEROY <fleroy@synergy.fr>
2023-08-29 17:17:38 +0200

    Test4

--- main.tf
+++ main.tf
@@ -22,13 +22,27 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
-resource "snowflake_schema" "SCH_DEMO01" {
-  database = snowflake_database.demo_db.name
-  name     = "SCH_DEMO01"
-  comment  = "Schema for Snowflake Terraform demo"
-}
 resource "snowflake_schema" "SCH_DEMO02" {
   database = snowflake_database.demo_db.name
-  name     = "SCH$A02"
+  name     = "SCH_DEMO02"
   comment  = "Schema for Snowflake Terraform demo"
+}
+resource "snowflake_table" "TB_DEMO01" {
+  database = snowflake_database.demo_db.name
+  schema   = snowflake_schema.SCH_DEMO01.name
+  name     = "TB_DEMO01"
+  columns = [
+    {
+      name     = "id"
+      type     = "integer"
+      nullable = false
+    },
+    {
+      name     = "name"
+      type     = "string"
+      length   = 256
+      nullable = false
+    }
+  ]
+  primary_key = ["id"]
 }
