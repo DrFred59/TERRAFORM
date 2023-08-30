2cfab21015c55cb524969e45d7a2472368383f9e
Frederic LEROY <fleroy@synergy.fr>
2023-08-30 11:11:58 +0200

    Test avec fichier test.tf (2)

--- test.tf
+++ test.tf
@@ -1,40 +1,6 @@
-terraform {
-  required_providers {
-    snowflake = {
-      source  = "chanzuckerberg/snowflake"
-      version = "0.25.17"
-    }
-  }
-
-  backend "remote" {
-    organization = "TEST_SYNERGY"
-
-    workspaces {
-      name = "gh-actions-demo"
-    }
-  }
-}
-
-provider "snowflake" {
-}
-
-resource "snowflake_database" "demo_db" {
-  name    = "DEMO_DB2"
-  comment = "Database for Snowflake Terraform demo"
-}
-resource "snowflake_schema" "SCH_DEMO01" {
-  database = snowflake_database.demo_db.name
-  name     = "SCH_DM01"
-  comment  = "Schema for Snowflake Terraform demo"
-}
-resource "snowflake_schema" "SCH_DEMO03" {
-  database = snowflake_database.demo_db.name
-  name     = "SCH_DM03"
-  comment  = "Schema for Snowflake Terraform demo"
-}
 resource "snowflake_table" "TB_DEMO03" {
   database = snowflake_database.demo_db.name
-  schema   = snowflake_schema.SCH_DEMO03.name
+  schema   = snowflake_schema.SCH_DEMO01.name
   name     = "TB_DEMO03"
 
   column {
